11ff6f73f5588d06d858b4cc50c752d3e5487144
Jane Greene <janegreenecasa@gmail.com>
2020-10-25 16:22:45 -0600

    import os for deployment test

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/career_day"
