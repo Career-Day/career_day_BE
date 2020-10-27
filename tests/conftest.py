ff986d90a4affbbe451ecaa62a1e3ac620cfe324
Phillip Strom <phillip.strom@gmail.com>
2020-10-27 16:48:50 -0600

    Create conftest
    Co-Authored-By: Dan Sehl <dtsehl@users.noreply.github.com>
    Co-Authored-By: Jane Greene <janegreenecasa@gmail.com>

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from app import create_app, db
+
+@pytest.fixture
+def app():
+    app = create_app()
+    app.config.from_object('config.TestingConfig')
+    with app.app_context():
+        db.create_all()
+        yield app
+        db.session.close()
+        db.drop_all()
