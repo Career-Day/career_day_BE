8cda633bd4558c135d564c55c18f72f84c76853f
Jane Greene <janegreenecasa@gmail.com>
2020-10-29 17:34:40 -0600

    trying new import scheme for python 3.9 debugger

--- tests/test_jobs.py
+++ tests/test_jobs.py
@@ -1,7 +1,21 @@
+
 import json
 import pytest
+import os
+import sys
+
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+
 from app import db, JobsModel
 
+def test_hello(app):
+  client = app.test_client()
+  resp = client.get('/')
+  import pdb; pdb.set_trace()
+  data = json.loads(resp.data.decode())
+  assert resp.status_code == 200
 
 def test_jobs(app):
     client = app.test_client()
